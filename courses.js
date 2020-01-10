315c873697344d6d7f3c785c17bba7709ac096ad
Seb Jeshurun <52677706+Seb-Jesh@users.noreply.github.com>
2020-01-10 05:25:00 +0200

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,23 @@
+courses = [
+{
+id: 1,
+title: 'The Complete JavaScript Course 2020',
+instructor: 'Jonas Schmedtmann',
+platform: 'Udemy',
+completed: false
+},
+{
+id: 2,
+title: 'Your Last Intro to Programming Course',
+instructor: 'Jake Overall, Darryl Kilzer',
+platform: 'Udemy',
+completed: false
+},
+{
+id: 3,
+title: 'The Modern React Bootcamp',
+instructor: 'Colt Steele',
+platform: 'Udemy',
+completed: false
+}
+]
